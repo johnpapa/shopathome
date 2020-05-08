dc00cccddb55b33360d66833e5c8bf874741f64d
John Papa <john@johnpapa.net>
2020-05-08 16:36:36 -0400

    >svelte is ready?

--- svelte-app/src/components/NavBar.svelte
+++ svelte-app/src/components/NavBar.svelte
@@ -1,9 +1,24 @@
 <script>
+  import { getContext, onMount } from 'svelte';
   import { Router, Link, Route } from 'svelte-routing';
-  import { getContext } from 'svelte';
   import { ROUTER } from 'svelte-routing/src/contexts';
 
   const { activeRoute } = getContext(ROUTER);
+  let userInfo = undefined;
+
+  onMount(async () => (userInfo = await getUserInfo()));
+
+  async function getUserInfo() {
+    try {
+      const response = await fetch('/.auth/me');
+      const payload = await response.json();
+      const { clientPrincipal } = payload;
+      return clientPrincipal;
+    } catch (error) {
+      console.error('No profile could be found');
+      return undefined;
+    }
+  }
 
   function getProps({ location, href, isPartiallyCurrent, isCurrent }) {
     const isActive = href === '/' ? isCurrent : isPartiallyCurrent || isCurrent;
@@ -16,11 +31,32 @@
   }
 </script>
 
-<nav class="column is-2 menu">
-  <p class="menu-label">Menu</p>
-  <ul class="menu-list">
-    <Link to="/home" {getProps}>Home</Link>
-    <Link to="/products" {getProps}>Products</Link>
-    <Link to="/discounts" {getProps}>Discounts</Link>
-  </ul>
-</nav>
+<div>
+  <nav class="column is-2 menu">
+    <p class="menu-label">Menu</p>
+    <ul class="menu-list">
+      <Link to="/home" {getProps}>Home</Link>
+      <Link to="/products" {getProps}>Products</Link>
+      <Link to="/discounts" {getProps}>Discounts</Link>
+    </ul>
+  </nav>
+  <nav class="menu auth">
+    <p class="menu-label">Auth</p>
+    <ul class="menu-list">
+      <div class="auth">
+        {#if !userInfo}
+          <a href="/login/tw">Twitter</a>
+          <a href="/login/gh">GitHub</a>
+          <a href="/.auth/login/facebook">FaceBook</a>
+        {/if}
+        {#if userInfo}
+          <a href="/logout">Logout</a>
+          <div class="user">
+            <p>{userInfo && userInfo.userDetails}</p>
+            <p>({userInfo && userInfo.identityProvider})</p>
+          </div>
+        {/if}
+      </div>
+    </ul>
+  </nav>
+</div>
