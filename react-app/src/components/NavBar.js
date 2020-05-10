588b6abe5e6b7413f37698c8d4b18740147f8ba0
John Papa <john@johnpapa.net>
2020-05-09 20:00:57 -0400

    linter

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useLocation, useHistory } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import { NavLink } from 'react-router-dom';
 
 function CurrentRoute(props) {
