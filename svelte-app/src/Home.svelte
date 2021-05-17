86ae9aa5e87e0516f680f3cd3f4ede763b85493c
John Papa <john@johnpapa.net>
2021-05-17 11:40:17 -0400

    changing to demo

--- svelte-app/src/Home.svelte
+++ svelte-app/src/Home.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
   import { Link } from 'svelte-routing';
+
 </script>
 
 <div class="content-container">
   <div class="content-title-group">
     <h2 class="title">Shop at Home</h2>
     <p>
-      Manage your shopping list! Become a preferred customer and gain access to
-      discount codes, too.
+      Hello Techorama! Manage your shopping list! Become a preferred customer
+      and gain access to discount codes, too.
     </p>
     <p>Log in to start enjoying your benefits.</p>
     <br />
@@ -25,7 +26,8 @@
         <a
           href="https://github.com/johnpapa/shopathome"
           target="_blank"
-          rel="noopener noreferrer">
+          rel="noopener noreferrer"
+        >
           <i class="fab fa-github" />
           Code in GitHub
         </a>
