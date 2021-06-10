7e7deaeb2e805c960dd0a9c30993c5538e641fd2
John Papa <john@johnpapa.net>
2021-06-10 07:23:43 -0400

    remove log statement

--- svelte-app/src/config.ts
+++ svelte-app/src/config.ts
@@ -2,7 +2,6 @@ const API: string = process.env.SVELTE_APP_API || 'api';
 const DEV: boolean = process.env.DEV || true;
 
 function logRouteLocation(location: Object) {
-  console.log(DEV);
   if (process.env.ROLLUP_WATCH) {
     console.table(location);
   }
