1fa9923fb38b67e3b505d2e563f551124b214439
John Papa <john@johnpapa.net>
2021-06-10 09:59:10 -0400

    fix home page

--- svelte-app/src/Home.svelte
+++ svelte-app/src/Home.svelte
@@ -7,7 +7,7 @@
   <div class="content-title-group">
     <h2 class="title">Shop at Home</h2>
     <p>
-      Hello DevTalks! Manage your shopping list! Become a preferred customer and gain access to
+      Manage your shopping list! Become a preferred customer and gain access to
       discount codes, too.
     </p>
     <p>Log in to start enjoying your benefits.</p>
