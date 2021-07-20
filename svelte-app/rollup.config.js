01c8e17d01ce217da86c0f3e3292eb8c41bcf612
John Papa <johnpapa@users.noreply.github.com>
2021-07-20 15:09:37 -0400

    yep (#54)

--- svelte-app/rollup.config.js
+++ svelte-app/rollup.config.js
@@ -9,8 +9,11 @@ import typescript from '@rollup/plugin-typescript';
 import replace from '@rollup/plugin-replace';
 
 const production = !process.env.ROLLUP_WATCH;
-const api = 'http://localhost:7071/api';
-const API = process.env.API || production ? '/api' : api;
+// If I want to run this locally without SWA proxying, do this
+// const api = 'http://localhost:7071/api';
+// If I want to run this locally with SWA proxying, do this
+const api = '/api';
+const API = process.env.API || production ? 'pro/api' : api;
 
 function serve() {
   let server;
