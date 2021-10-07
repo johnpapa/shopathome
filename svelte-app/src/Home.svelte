3c00abeb3519a3436fceaf3a0ab9a0ce370c4844
John Papa <john@johnpapa.net>
2021-10-07 15:03:58 -0400

    .

--- svelte-app/src/Home.svelte
+++ svelte-app/src/Home.svelte
@@ -5,7 +5,7 @@
 <div class="content-container">
   <div class="content-title-group">
     <h2 class="title">Shop at Home</h2>
-    <h3>Goodbye Azure Thursday</h3>
+    <h3>Goodbye Azure Thursday!</h3>
     <p>
       Manage your shopping list! Become a preferred customer and gain access to
       discount codes, too.
