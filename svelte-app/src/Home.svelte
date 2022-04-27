12f595315584f88e3b8d27a1113b739d97ad3581
John Papa <john@johnpapa.net>
2022-04-27 17:49:00 -0400

    updated the welcome message to USF

--- svelte-app/src/Home.svelte
+++ svelte-app/src/Home.svelte
@@ -6,8 +6,8 @@
   <div class="content-title-group">
     <h2 class="title">Shop at Home</h2>
     <p>
-      Manage your shopping list! Become a preferred customer and gain access to
-      discount codes, too.
+      Hello USF! Manage your shopping list! Become a preferred customer and gain
+      access to discount codes, too.
     </p>
     <p>Log in to start enjoying your benefits</p>
     <br />
