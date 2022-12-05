3bd07bd9e12da88b063a6a44a199d1f83adb5cbc
John Papa <john@johnpapa.net>
2022-12-05 16:47:17 -0500

    svelte now using vite

--- svelte-app/src/store/store.ts
+++ svelte-app/src/store/store.ts
@@ -1,4 +1,5 @@
-import { Writable, writable } from 'svelte/store';
+import type { Writable } from 'svelte/store';
+import { writable } from 'svelte/store';
 import { Discount, Product } from '../models';
 
 interface AppState {
