8f9d34dbb8df8e90d11a38ed5450a2948b705ccd
John Papa <john@johnpapa.net>
2022-12-05 17:43:30 -0500

    fixing env variables so the API gets picked up. not sure if its right yet, need to test in azure

--- svelte-app/src/config.ts
+++ svelte-app/src/config.ts
@@ -1,8 +1,8 @@
-// const API: string = process.env.SVELTE_APP_API || 'api';
-// const DEV: boolean = process.env.DEV || true;
+const API: string = import.meta.env.SVELTE_APP_API || 'api';
+const DEV: boolean = import.meta.env.DEV || true;
 
-const API: string = 'http://localhost:7071/api';
-const DEV: boolean = true;
+// const API: string = 'http://localhost:7071/api';
+// const DEV: boolean = true;
 
 function logRouteLocation(location: Object) {
   console.log(DEV);
