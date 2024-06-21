f8b9e131df25620bfc801e8207cd3a7f1acf0b69
johnpapa <john@johnpapa.net>
2024-06-21 11:34:46 -0400

    refactor: Remove console.log statement in discounts route

--- fastify-api-server/src/routes/discounts.js
+++ fastify-api-server/src/routes/discounts.js
@@ -4,7 +4,6 @@ async function routes(fastify, options) {
   // add route to get all discounts using the data module
   fastify.get('/discounts', async (request, reply) => {
     const discounts = data.getDiscounts();
-    console.log('discounts:', discounts);
     return discounts;
   });
 }
