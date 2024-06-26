374e8e3402df7f3780a165010adad2d1de2191c2
johnpapa <john@johnpapa.net>
2024-06-26 10:34:11 -0400

    refactor: Remove FASTIFY flag from config.ts

--- svelte-app/src/config.ts
+++ svelte-app/src/config.ts
@@ -5,15 +5,13 @@ const API = import.meta.env.VITE_API
   : production
     ? '/api'
     : api;
-const FASTIFY = import.meta.env.VITE_FASTIFY === 'true' ? true : false;
 const DEV: boolean = import.meta.env.DEV || true;
 
 function logEnvironment() {
   console.log('API:', API);
   console.log('DEV:', DEV);
-  console.log('FASTIFY:', FASTIFY);
 }
 
 logEnvironment();
 
-export { API, DEV, FASTIFY, logEnvironment };
+export { API, DEV, logEnvironment };
