cad332c51b1e4dbbede30ea9b32995457160d1cd
chris <noringc@gmail.com>
2024-07-09 18:16:48 +0000

    clean up router file

--- vue-app/src/router.js
+++ vue-app/src/router.js
@@ -1,5 +1,3 @@
-// import Vue from 'vue';
-// import Router from 'vue-router';
 import { createRouter, createWebHistory } from 'vue-router';
 import PageNotFound from './components/page-not-found.vue';
 
@@ -40,45 +38,3 @@ const router = createRouter({
 });
 
 export default router;
-
-// import PageNotFound from './components/page-not-found.vue';
-
-// Vue.use(Router);
-
-// export default new Router({
-//   mode: 'history',
-//   base: process.env.BASE_URL,
-//   routes: [
-//     {
-//       path: '/',
-//       redirect: '/home',
-//     },
-//     {
-//       path: '/products',
-//       name: 'products',
-//       component: () =>
-//         import(
-//           /* webpackChunkName: "products" */ './views/products/products.vue'
-//         ),
-//     },
-//     {
-//       path: '/discounts',
-//       name: 'discounts',
-//       component: () =>
-//         import(/* webpackChunkName: "discount" */ './views/discounts.vue'),
-//     },
-//     {
-//       path: '/home',
-//       name: 'home',
-//       // route level code-splitting
-//       // this generates a separate chunk (home.[hash].js) for this route
-//       // which is lazy-loaded when the route is visited.
-//       component: () =>
-//         import(/* webpackChunkName: "home" */ './views/home.vue'),
-//     },
-//     {
-//       path: '*',
-//       component: PageNotFound,
-//     },
-//   ],
-// });
